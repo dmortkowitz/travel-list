f9aee77d24610df302bcad70fbfff72a062c87de
Chris Crow, David Mortkowitz and Skye Nguyen <skye@>
2018-08-20 09:01:02 -0700

    Add list in HTML, added front-end JS.

--- js/scripts.js
+++ js/scripts.js
@@ -2,3 +2,17 @@ function Location (city, country) {
   this.cityName = city;
   this.countryName = country;
 }
+
+$(document).ready(function(){
+  $("form#new-location").submit(function(event) {
+    event.preventDefault();
+
+    var inputtedCity = $("input#new-cityLocation").val();
+    var inputtedCountry = $("input#new-countryLocation").val();
+
+    var newLocation = new Location(inputtedCity, inputtedCountry);
+    $("")
+
+
+  });
+});
