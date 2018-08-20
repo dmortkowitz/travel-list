b8ceeaeab18a255231f6f2d97b9ba17f42089db9
Chris Crow, David Mortkowitz and Skye Nguyen <chris@>
2018-08-20 09:05:33 -0700

    Add last of initial JS script.

--- js/scripts.js
+++ js/scripts.js
@@ -1,8 +1,12 @@
+//backend logic
+
 function Location (city, country) {
   this.cityName = city;
   this.countryName = country;
 }
 
+
+//frontend logic
 $(document).ready(function(){
   $("form#new-location").submit(function(event) {
     event.preventDefault();
@@ -11,8 +15,10 @@ $(document).ready(function(){
     var inputtedCountry = $("input#new-countryLocation").val();
 
     var newLocation = new Location(inputtedCity, inputtedCountry);
-    $("")
+    $("ul#locations").append("<li><span class='contact'>" + newLocation.cityName + "</span></li>");
 
+    $("input#new-cityLocation").val();
+    $"input#new-countryLocation").val();
 
   });
 });
